75ce41a230bc8290677727192d43af646d2d42f0
ichigo-k <tettesaintluke@gmail.com>
2023-03-10 22:53:45 +0000

    Initial commit

---
+++ python-basics/main.py
@@ -0,0 +1,15 @@
+#OUTPUTS IN PYTHON
+print("HELLO WORLD")
+
+#USING VARIABLES
+age=12
+name="Kephas"
+school="GCTU"
+print(f"Hello {name},your age is {age} and you are a student of {school}")
+
+#ACCEPTING USER INPUT
+print("USER DETAILS")
+name=input("Please enter your fulname\n")
+age=input("What is your age\n")
+school=input("Enter the name of your school\n")
+print(f"Dear {name},Thanks for filling out the form.Are you sure you are {age} years old.If yes a link will be sent to {school} portal")
